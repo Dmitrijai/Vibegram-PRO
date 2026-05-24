69fbbc88ce6894b1dc672637ec8262b9be38f26f
Dmitrijai <dmitrijnewai@gmail.com>
2026-05-24 20:03:01 +0200

    feat: add TURN server configuration to WebRTC
    Include openrelay.metered.ca TURN servers to improve connectivity in restricted network environments.

--- src/webrtc.ts
+++ src/webrtc.ts
@@ -64,6 +64,15 @@ const rtcConfig: RTCConfiguration = {
         "stun:stun4.l.google.com:19302",
       ],
     },
+    {
+      urls: [
+        "turn:openrelay.metered.ca:80",
+        "turn:openrelay.metered.ca:443",
+        "turn:openrelay.metered.ca:443?transport=tcp",
+      ],
+      username: "openrelayproject",
+      credential: "openrelayproject"
+    }
   ],
 };
 
