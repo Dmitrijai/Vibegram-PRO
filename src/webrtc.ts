ed32cd2016c0c43f98dc526ef1e82fb5eab06bb2
Dmitrijai <dmitrijnewai@gmail.com>
2026-05-25 18:07:24 +0200

    feat: add additional TURN servers for WebRTC
    Update RTCConfiguration to include more reliable TURN server relay options to improve connectivity.

--- src/webrtc.ts
+++ src/webrtc.ts
@@ -50,7 +50,11 @@ const rtcConfig: RTCConfiguration = {
         { urls: 'stun:stun.l.google.com:19302' },
         { urls: 'stun:stun1.l.google.com:19302' },
         { urls: 'stun:stun2.l.google.com:19302' },
-        { urls: 'stun:stun.cloudflare.com:3478' }
+        { urls: 'stun:stun.cloudflare.com:3478' },
+        { urls: 'turn:openrelay.metered.ca:80', username: 'openrelayproject', credential: 'openrelayproject' },
+        { urls: 'turn:openrelay.metered.ca:443', username: 'openrelayproject', credential: 'openrelayproject' },
+        { urls: 'turn:openrelay.metered.ca:443?transport=tcp', username: 'openrelayproject', credential: 'openrelayproject' },
+        { urls: 'turn:freestun.net:3478', username: 'free', credential: 'free' }
     ]
 };
 
